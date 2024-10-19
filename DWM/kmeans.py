503ea208c998f200e79462e1dd26aec1e86c1c7e
TushitPalamkar <143091687+TushitPalamkar@users.noreply.github.com>
2024-10-19 15:31:58 +0530

    AI/DWM

--- DWM/kmeans.py
+++ DWM/kmeans.py
@@ -1,36 +1,31 @@
-import random 
+import random
 import statistics
 def distance(x,centroid):
     return abs(x-centroid)
-arr=[]
-n=int(input("Enter no of elements:"))
-for i in range(n):
-    arr.append(int(input(f"Element{i+1}:")))
-k=int(input("enter no of clusters:"))
-centroids=random.sample(arr,k)
-print(centroids)
-iteration=0
-while True:
-    iteration=iteration+1
-    print(f"Iteration:{iteration+1}")
-    cluster={i:[] for i in range(k)}
 
-    for j in arr:
-        distances=[distance(j,centroid) for centroid in centroids]
-        nearestdistance=distances.index(min(distances))
-        cluster[nearestdistance].append(j)
+arr=[]
+num=int(input("Enter size of the array:"))
+print("Enter the elements:\n")
+for i in range(num):
+    num = int(input(f"Element {i + 1}: "))
+    arr.append(num)
+
+k=int(input("Enter the no of clusters:"))
+centroids=random.sample(arr,k)
+while True:
+    clusters={i:[] for i in range(k)}
+    for x in arr:
+        distances=[distance(x,centroid) for centroid in centroids ]
+        nearest_centroid=distances.index(min(distances))
+        clusters[nearest_centroid].append(x)
     new_centroids=[]
     for i in range(k):
-        if cluster[i]:
-            new_centroids.append(statistics.mean(cluster[i]))
+        if clusters[i]:
+            new_centroids.append(statistics.mean(clusters[i]))
         else:
             new_centroids.append(centroids[i])
     
-    print("Clusters:",cluster)
-    print("Centroids:",new_centroids)
+    print(f"Clusters:{clusters} | centroids:{new_centroids}")
     if new_centroids==centroids:
         break
     centroids=new_centroids
-
-print("\nFinal clusters:", cluster)
-print("Final centroids:", centroids)
--- DWM/page_rank.py
+++ DWM/page_rank.py
@@ -1,3 +1,29 @@
-from oripy import CodeLibrary
-library=CodeLibrary()
-library.id3()
+def page_rank(graph,max_iterations,damping_factor=0.85):
+    num_pages=len(graph)
+    pr={page:1/num_pages for page in graph}
+    for _ in range(max_iterations):
+        new_pr={}
+        for page in graph:
+            incoming_pr=sum(pr[i]/len(graph[i]) for i in graph if page in graph[i])
+            new_pr[page]=(1-damping_factor)*damping_factor+incoming_pr
+        pr=new_pr
+    for page,rank in pr.items():
+        print(f"{page}:{rank}")
+    return pr
+
+def get_graph_input():
+    graph={}
+    while True:
+        page=input("Enter the page:")
+        if not page:
+            break
+        links=input(f"Enter the links associated with {page}:").split(',')
+        graph[page]=[link.strip() for link in links if link.strip()]
+    return graph
+print("Enter the graph structure:")
+graph=get_graph_input()
+iter=int(input("Iterations:"))
+result=page_rank(graph,iter)
+for page,rank in result.items():
+    print(f"{page}:{rank}")
+
